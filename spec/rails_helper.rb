2e83bd061c4e153fb2ec9cbd5bd656c5254cd032
Edivan de Castro <edivandecastro@gmail.com>
2023-05-15 23:19:25 -0300

    Rodando os testes de forma aleatoria

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,6 +59,9 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+
+  config.order = :rand
+
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
