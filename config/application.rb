83cca85d5e49ff27418e7b5c2b344a961ea8cab5
Edivan de Castro <edivandecastro@gmail.com>
2023-05-15 23:20:47 -0300

    Configurado FactoryBot

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module InvestmentApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+      g.factory_bot suffix: "factory"
+    end
   end
 end
