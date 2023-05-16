c37542f1ded8eaf39e8904888c1e9193d9ef82b5
Edivan de Castro <edivandecastro@gmail.com>
2023-05-16 12:42:06 -0300

    Permitindo acesso do IP do servidor em produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << ENV.fetch("ALLOWED_SERVER") { "localhost" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
