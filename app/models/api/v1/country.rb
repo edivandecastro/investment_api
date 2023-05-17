3b220cfb38e9920a3f44dd71aaad3df513a69e12
Edivan de Castro <edivandecastro@gmail.com>
2023-05-17 12:57:42 -0300

    Adicionado validação dos campos de country

--- app/models/api/v1/country.rb
+++ app/models/api/v1/country.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class Country < ApplicationRecord
       self.implicit_order_column = "created_at"
+
+      validates :name, :locale, :acronym, presence: true
     end
   end
 end
