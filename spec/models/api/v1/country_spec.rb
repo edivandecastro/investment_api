3bb1cde697c7defa097fe92453b39e4f5bee9f39
Edivan de Castro <edivandecastro@gmail.com>
2023-05-17 12:58:09 -0300

    Implementado testes de modelo de country

--- spec/models/api/v1/country_spec.rb
+++ spec/models/api/v1/country_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "factory" do
+    expect(FactoryBot.build(:country)).to be_valid
+  end
+
+  context "columns" do
+    it { is_expected.to have_db_column(:name).of_type(:string) }
+    it { is_expected.to have_db_column(:locale).of_type(:string) }
+    it { is_expected.to have_db_column(:acronym).of_type(:string) }
+  end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:locale) }
+    it { is_expected.to validate_presence_of(:acronym) }
+  end
 end
