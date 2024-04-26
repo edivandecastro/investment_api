8c783c7c400ed80e2f13bad2b3a65de58545d122
Edivan de Castro <edivandecastro@gmail.com>
2024-04-26 09:09:34 -0300

    Verificado tempo de expiração de token no teste

--- spec/commands/validate_token_spec.rb
+++ spec/commands/validate_token_spec.rb
@@ -3,11 +3,19 @@ require 'rails_helper'
 RSpec.describe ValidateToken, type: :model do
   describe ".call" do
     context "when token is invalid" do
-      it "raise ServiceActor::Failure exception" do
+      it "raise ServiceActor::Failure exception with malformed_token message" do
         error_message = I18n.t('general.decode_error')
 
         expect {
           ValidateToken.call(token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9")
+        }.to raise_error(ServiceActor::Failure, error_message)
+      end
+
+      it "raise ServiceActor::Failure exception with expired_token message" do
+        error_message = I18n.t('general.expired_token')
+
+        expect {
+          ValidateToken.call(token: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMWY0NTcwMy0wZTBkLTQ5NjYtYjY2ZC0zMmUzNWFkOTRlNjYiLCJleHAiOjE3MTQwNjE0MTcsImlzcyI6IklOVkVTVE1FTlQifQ.CiXECj9_0jraXLvZTWzbQhueF80ON_S-7TCidXN6j8k")
         }.to raise_error(ServiceActor::Failure, error_message)
       end
     end
