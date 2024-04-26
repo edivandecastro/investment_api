a84bd7929a3a363f764e6b51e5b091b82688fbe2
Edivan de Castro <edivandecastro@gmail.com>
2024-04-26 09:12:08 -0300

    Adicionado rotas de patrimony como e position_import na collection de users

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      devise_for :user, class_name: Api::V1::User
       resources :countries
       resources :assets
       resources :sectors
       resources :stocks
       resources :user_assets
-      devise_for :user, class_name: Api::V1::User do
+      resources :users do
         resources :stocks
 
-        post 'position_import', on: :member
-        get 'patrimony', on: :member
+        collection do
+          post 'position_import'
+          get 'patrimony'
+        end
       end
     end
   end
